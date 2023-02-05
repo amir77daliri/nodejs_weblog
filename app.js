9e7f2f89d978bf9a6037f1a5927a33e6f1daca21
Amirhossein_Daliri <amirdalir0111@gmail.com>
2023-02-05 13:09:55 +0330

    config mongo db with mongoose and bootstrap && font-awesome

--- app.js
+++ app.js
@@ -3,25 +3,37 @@ const path = require('path');
 const express = require('express');
 const dotEnv = require('dotenv');
 
+const connectDB = require('./config/db');
 const indexRoutes = require('./routes/index');
+
 
 // Load Config --> 
 dotEnv.config({path: "./config/config.env"})
 // 
 
+
+// DataBase connection
+connectDB();
+
 const app = express();
+
 
 // View Engine :
 app.set("view engine", "ejs");
 app.set("views", "views");
 // End View Engine Config
 
+
 // Set Static
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "node_modules", "bootstrap-v4-rtl", "dist")))
+app.use(express.static(path.join(__dirname, "node_modules", "font-awesome")))
+
 
 // Start routes
 app.use(indexRoutes);
 // End routes
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}...`))
